a07666810e1b126fb1c9413c9c4337d987427cd1
Sukhrob Khakimov <sukhrob.khakimov@gmail.com>
2021-01-12 19:53:35 +0500

    Create Templating protocol as a common interface for all templating language implementations base on SwiftNIO

--- Sources/Templating/Templating.swift
+++ Sources/Templating/Templating.swift
@@ -1,1 +1,23 @@
-struct Templating {}
+import NIO
+
+public protocol Templating {
+    func loadTemplate(named name: String) throws -> EventLoopFuture<Template>
+    func loadTemplate(at path: String) throws -> EventLoopFuture<Template>
+    func renderTemplate(named name: String, context: [String: Encodable]) throws -> EventLoopFuture<String>
+    func renderTemplate(at path: String, context: [String: Encodable]) throws -> EventLoopFuture<String>
+    func renderTemplate(_ template: Template, context: [String: Encodable]) throws -> EventLoopFuture<String>
+}
+
+extension Templating {
+    public func renderTemplate(named name: String) throws -> EventLoopFuture<String> {
+        try renderTemplate(named: name, context: .init())
+    }
+
+    public func renderTemplate(at path: String) throws -> EventLoopFuture<String> {
+        try renderTemplate(at: path, context: .init())
+    }
+
+    public func renderTemplate(_ template: Template) throws -> EventLoopFuture<String> {
+        try renderTemplate(template, context: .init())
+    }
+}
